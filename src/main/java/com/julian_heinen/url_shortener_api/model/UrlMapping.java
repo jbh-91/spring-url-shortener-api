7b12769d16fb3b032be30c583ceee75b8dfe6029
Julian Heinen <mail@julian-heinen.de>
2025-12-21 21:03:52 +0100

    Add model

---
+++ src/main/java/com/julian_heinen/url_shortener_api/model/UrlMapping.java
@@ -0,0 +1,26 @@
+package com.julian_heinen.url_shortener_api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class UrlMapping {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @NonNull // Damit @RequiredArgsConstructor es nutzt
+    private String originalUrl;
+}

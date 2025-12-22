f528d2fc9c039caf24532cab257d185037a8f799
Julian Heinen <mail@julian-heinen.de>
2025-12-22 22:28:55 +0100

    Add URL validation in controller

--- src/main/java/com/julian_heinen/url_shortener_api/controller/UrlShortenerController.java
+++ src/main/java/com/julian_heinen/url_shortener_api/controller/UrlShortenerController.java
@@ -1,12 +1,15 @@
 package com.julian_heinen.url_shortener_api.controller;
 
+import org.hibernate.validator.constraints.URL;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.julian_heinen.url_shortener_api.service.UrlShortenerService;
@@ -15,12 +18,14 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping // not specifying a path here to allow easy configuration in the future
+@Validated
 public class UrlShortenerController {
 
     private final UrlShortenerService urlShortenerService;
 
     @PostMapping
-    public ResponseEntity<String> shortenUrl(@RequestBody String longUrl) {
+    public ResponseEntity<String> shortenUrl(@RequestBody @URL String longUrl) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(urlShortenerService.shortenUrl(longUrl));
     }
